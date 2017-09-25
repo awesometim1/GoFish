bc2e7f2e290ec4931980c4f92e4522f387c81258
amherms <32274843+amherms@users.noreply.github.com>
2017-09-25 14:00:16 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,41 @@
+/**
+ * Anna Herms, Zach Richey, Tim Lee
+ * Edited for GO FISH
+ */
+public class Card 
+{
+
+	private String suit;//String value that holds the suit of the card
+
+	private String rank;//String value that holds the rank of the card
+
+	public Card(String cardRank, String cardSuit) 
+   {
+		suit = cardSuit;
+      rank = cardRank;
+  	}
+
+	public String getSuit() 
+   {
+		return this.suit;
+   }
+
+	public String getRank() 
+   {
+		return this.rank;
+   }
+
+	public boolean matches(Card otherCard) 
+   {
+      if((this.rank()).compareTo(otherCard.rank())!= 0)
+         return false;
+      else 
+         return true;
+	}
+
+	@Override
+	public String toString() 
+   {
+		return this.rank() + " of " + this.suit();
+	}
+}
