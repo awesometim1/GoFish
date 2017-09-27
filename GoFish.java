840b10a5709eab14018ad98c5f35ef28e67af5eb
awesometim1 <awesometim1@gmail.com>
2017-09-27 14:13:02 -0400

    Final Change

--- GoFish.java
+++ GoFish.java
@@ -14,31 +14,61 @@ public class GoFish{
          {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
       String[] suits52 =
          {"spades", "hearts", "diamonds", "clubs"};
-      int[] pointValues52 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-      dMain = new Deck(ranks52, suits52, pointValues52);
+      dMain = new Deck(ranks52, suits52);
       dMain.shuffle();
-   
+      
+      
+      
+      Player robot = new Player(dMain.deal(5));
+      Player human = new Player(dMain.deal(5));
    
    
    }
    
-   
-   public static void initialize(){
-      Player robot = new Player();
-      Player user = new Player();
-      for (int i = 0; i < 5; i ++){
-      robot.hand.add(dMain.deal());
-      user.hand.add(dMain.deal());
+   public void turn(){
+      human.checkPairInitial();
+      robot.checkPairInitial();
+      Scanner in = new Scanner(System.in);
+      while (true){
+      
+      //Human Behavior
+         System.out.println("Enter a card to ask numbers 1 - 13");
+         int input = in.nextInt();
+         human.ask(input, robot, dMain);
+         human.checkPair();
+         human.showHand();
+      
+      //Check if no remaining cards
+      if (human.hand.size() < 1 ){
+         if (human.pairs > robot.pairs)
+         System.out.println("Human Has WON!");
+         else if (human.pairs < robot.pairs)
+         System.out.println("Robot has WON");
+         else
+         System.out.println("It's a tie!");
+         break;
       }
+      //Robot Behavior
+         int rand = (int)(Math.Random() * 13 + 1);
+         robot.ask(rand, human, dMain);
+         robot.checkPair();
+         robot.showHand();
+         
+       //Check if no remaining cards
+      if (robot.hand.size() < 1){
+         if (human.pairs > robot.pairs)
+         System.out.println("Human Has WON!");
+         else if (human.pairs < robot.pairs)
+         System.out.println("Robot has WON");
+         else
+         System.out.println("It's a tie!");
+         break;
       }
-   public static void turn(){
-   boolean turn = false;
-   if false
-      player
-      turn true
-   else 
-      robot 
+      
+      }
+   }
    
-   }
-   }
+   
+   
+  
 }
